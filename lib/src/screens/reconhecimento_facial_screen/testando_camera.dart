442a0fe8d5b8a84685dcd951efc82d499d790f1e
matheuscrdoso <cmatheusm13@gmail.com>
2024-02-09 15:40:24 -0300

    fim

--- lib/src/screens/reconhecimento_facial_screen/testando_camera.dart
+++ lib/src/screens/reconhecimento_facial_screen/testando_camera.dart
@@ -5,96 +5,59 @@ class TelaReconhecimentoFacialWidget extends StatefulWidget {
   const TelaReconhecimentoFacialWidget({Key? key}) : super(key: key);
 
   @override
-  State<TelaReconhecimentoFacialWidget> createState() => _TelaReconhecimentoFacialWidgetState();
+  State<TelaReconhecimentoFacialWidget> createState() =>
+      _TelaReconhecimentoFacialWidgetState();
 }
 
-class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacialWidget> {
-  late CameraController _controller;
+class _TelaReconhecimentoFacialWidgetState
+    extends State<TelaReconhecimentoFacialWidget> {
+  late CameraController _cameraController;
+  Future<void>? _initializeControllerFuture;
 
-//SE FOR RODAR O CODIGO NO PC (WEB) USAR ESSE CODIGO =================================================
+ @override
+void initState() {
+  super.initState();
+  _initializeControllerFuture = _initializeCamera();
+}
 
-@override
-  void initState() {
-    super.initState();
-    _initializeCamera();
+Future<void> _initializeCamera() async {
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  _cameraController = CameraController(
+    firstCamera,
+    ResolutionPreset.medium,
+  );
+
+  return _cameraController.initialize();
+}
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
   }
-
-  Future<void> _initializeCamera() async {
-    final cameras = await availableCameras();
-    if (cameras.isEmpty) {
-      // Caso não haja câmeras disponíveis, você pode tratar isso aqui
-      print('Nenhuma câmera encontrada.');
-      return;
-    }
-    final firstCamera = cameras.first;
-    _controller = CameraController(firstCamera, ResolutionPreset.medium);
-    await _controller.initialize();
-    if (mounted) {
-      setState(() {});
-    }
-  }
-//// ATE AQUI O DO PC WEB^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-
-////SE FOR RODAR O CODIGO NO CELULAR USAR ESSE CODIGO =================================================
-// @override
-// void initState() {
-//   super.initState();
-//   _initializeCamera();
-// }
-
-// Future<void> _initializeCamera() async {
-//   final cameras = await availableCameras();
-//   CameraDescription? backCamera; // Mudança aqui
-
-//   for (final camera in cameras) {
-//     if (camera.lensDirection == CameraLensDirection.back) { // Mudança aqui
-//       backCamera = camera; // Mudança aqui
-//       break;
-//     }
-//   }
-
-//   if (backCamera == null) {
-//     throw CameraException('Câmera traseira não encontrada.', 'cameraNotFound'); // Mudança aqui
-//   }
-
-//   _controller = CameraController(backCamera, ResolutionPreset.medium); // Mudança aqui
-//   await _controller.initialize();
-//   if (mounted) {
-//     setState(() {});
-//   }
-// }
-//// ATE AQUI O DO CELULAR ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    if (_controller == null || !_controller.value.isInitialized) {
-      return const Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
+    //FUNDO VERDE //////////////////////////////////////////////////////////////////////////
     return Scaffold(
       backgroundColor: const Color(0xFF236742),
       body: SafeArea(
         top: true,
         child: Stack(
           children: [
-            // Seu código existente para o background branco
             Positioned(
               bottom: 0,
               left: 0,
               child: Container(
                 width: screenWidth,
-                height: screenHeight > 855 ? screenHeight * 0.79 : screenHeight * 0.77, 
+                height: screenHeight > 855
+                    ? screenHeight * 0.86
+                    : screenHeight * 0.84,
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -102,28 +65,52 @@ class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacia
                     topRight: Radius.circular(30),
                   ),
                 ),
-              ),
-            ),
-            // Câmera integrada dentro do background branco
-            Positioned.fill(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox( // Aqui você pode ajustar a altura do Container
-                height: screenHeight > 855  ? screenHeight * 0.80 : screenHeight * 0.78, 
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: CameraPreview(_controller),
+                child: FutureBuilder<void>(
+                  future: _initializeControllerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return Container(
+                        height: screenHeight > 855
+                            ? screenHeight * 0.86
+                            : screenHeight * 0.84,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: AspectRatio(
+                            aspectRatio: _cameraController.value.aspectRatio,
+                            child: ClipRect(
+                              child: OverflowBox(
+                                alignment: Alignment.bottomCenter,
+                                maxHeight: double.infinity,
+                                maxWidth: double.infinity,
+                                child: FittedBox(
+                                  fit: BoxFit.cover,
+                                  child: SizedBox(
+                                    width: screenWidth,
+                                    height: screenHeight > 855
+                                        ? screenHeight * 0.86
+                                        : screenHeight * 0.84,
+                                    child: CameraPreview(_cameraController),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
                 ),
               ),
-              ),
             ),
+
             Center(
               child: CustomPaint(
                 size: Size(screenWidth, screenHeight),
-                painter: OvalPainter(),
+                painter: _OvalPainter(),
               ),
             ),
-             
             Align(
               alignment: Alignment.bottomCenter,
               child: Column(
@@ -131,7 +118,8 @@ class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacia
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.of(context).pushNamed('TelaReconhecimentoSucesso');
+                      Navigator.of(context)
+                          .pushNamed('TelaLoadingReconhecimento');
                     },
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
@@ -150,12 +138,10 @@ class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacia
                       ),
                     ),
                   ),
-                  const SizedBox(
-                      height: 20), // Ajuste esta altura conforme necessário
+                  const SizedBox(height: 20),
                 ],
               ),
             ),
-            // Seu código existente para o título e outros elementos
             Positioned(
               top: screenHeight * 0.02,
               left: 0,
@@ -168,7 +154,7 @@ class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacia
                   style: TextStyle(
                     fontFamily: 'Roboto',
                     color: Colors.white,
-                    fontSize: 25,
+                    fontSize: 31,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -190,21 +176,14 @@ class _TelaReconhecimentoFacialWidgetState extends State<TelaReconhecimentoFacia
                 constraints: const BoxConstraints(),
               ),
             ),
-            
           ],
         ),
       ),
     );
   }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 }
 
-class OvalPainter extends CustomPainter {
+class _OvalPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
@@ -212,11 +191,14 @@ class OvalPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4.0;
 
-    final horizontalRadius = size.width * 0.7; // Ajuste o raio horizontal conforme necessário
-    final verticalRadius = size.height * 0.5; // Ajuste o raio vertical conforme necessário
+    final horizontalRadius = size.width * 0.7; 
+    final verticalRadius = size.height * 0.5; 
 
     canvas.drawOval(
-      Rect.fromCenter(center: size.center(Offset.zero), width: horizontalRadius, height: verticalRadius),
+      Rect.fromCenter(
+          center: size.center(Offset.zero),
+          width: horizontalRadius,
+          height: verticalRadius),
       paint,
     );
   }
@@ -226,3 +208,4 @@ class OvalPainter extends CustomPainter {
     return false;
   }
 }
+
